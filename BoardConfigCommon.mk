f9338c83ee1c3cc1a537242bdc54d24315d30457
andr68rus <ashtmb@gmail.com>
2019-02-11 11:44:20 +0300

    msm8996-common: de-los

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -174,9 +174,6 @@ TARGET_RECOVERY_DEVICE_MODULES := libinit_leeco_msm8996
 # IPA
 USE_DEVICE_SPECIFIC_DATA_IPA_CFG_MGR := true
 
-# Lineage Hardware
-JAVA_SOURCE_OVERLAYS := org.lineageos.hardware|$(PLATFORM_PATH)/lineagehw|**/*.java
-
 # Partitions (/proc/partitions * 2 * BLOCK_SIZE (512) = size in bytes)
 BOARD_BOOTIMAGE_PARTITION_SIZE := 67108864
 BOARD_CACHEIMAGE_PARTITION_SIZE := 268435456
