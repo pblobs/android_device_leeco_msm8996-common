omnirom-op7/android_device_oneplus_oneplus7

policy_hal: fix build
changes in https://android.googlesource.com/platform/frameworks/av/+/1f76f20b2f14b2f08934380c65bf1495e29b969f%5E%21/#F4

Change-Id: I8c28274a81e495e22c3685ba7e7ca8bd3c0a7192

policy_hal/AudioPolicyManager.cpp
@@ -2245,9 +2245,9 @@ status_t AudioPolicyManagerCustom::startInput(audio_io_handle_t input,
 
         if (inputDesc->getAudioSessionCount(true/*activeOnly*/) == 1) {
             // if input maps to a dynamic policy with an activity listener, notify of state change
-            if ((inputDesc->mPolicyMix != NULL)
-                    && ((inputDesc->mPolicyMix->mCbFlags & AudioMix::kCbFlagNotifyActivity) != 0)) {
-                mpClientInterface->onDynamicPolicyMixStateUpdate(inputDesc->mPolicyMix->mDeviceAddress,
+            if ((inputDesc->mPolicyMix.promote() != NULL)
+                    && ((inputDesc->mPolicyMix.promote()->mCbFlags & AudioPolicyMix::kCbFlagNotifyActivity) != 0)) {
+                mpClientInterface->onDynamicPolicyMixStateUpdate(inputDesc->mPolicyMix.promote()->mDeviceAddress,
                         MIX_STATE_MIXING);
             }
 
@@ -2268,8 +2268,8 @@ status_t AudioPolicyManagerCustom::startInput(audio_io_handle_t input,
                 String8 address = String8("");
                 if (inputDesc->mPolicyMix == NULL) {
                     address = String8("0");
-                } else if (inputDesc->mPolicyMix->mMixType == MIX_TYPE_PLAYERS) {
-                    address = inputDesc->mPolicyMix->mDeviceAddress;
+                } else if (inputDesc->mPolicyMix.promote()->mMixType == MIX_TYPE_PLAYERS) {
+                    address = inputDesc->mPolicyMix.promote()->mDeviceAddress;
                 }
                 if (address != "") {
                     setDeviceConnectionStateInt(AUDIO_DEVICE_OUT_REMOTE_SUBMIX,

pblobs/android_device_leeco_msm8996-common

policy_hal/AudioPolicyManager.cpp
@@ -2175,9 +2175,9 @@ status_t AudioPolicyManagerCustom::startInput(audio_io_handle_t input,
 
         if (inputDesc->getAudioSessionCount(true/*activeOnly*/) == 1) {
             // if input maps to a dynamic policy with an activity listener, notify of state change
-            if ((inputDesc->mPolicyMix != NULL)
-                    && ((inputDesc->mPolicyMix->mCbFlags & AudioMix::kCbFlagNotifyActivity) != 0)) {
-                mpClientInterface->onDynamicPolicyMixStateUpdate(inputDesc->mPolicyMix->mDeviceAddress,
+            if ((inputDesc->mPolicyMix.promote() != NULL)
+                    && ((inputDesc->mPolicyMix.promote()->mCbFlags & AudioMix::kCbFlagNotifyActivity) != 0)) {
+                mpClientInterface->onDynamicPolicyMixStateUpdate(inputDesc->mPolicyMix.promote()->mDeviceAddress,
                         MIX_STATE_MIXING);
             }
 
@@ -2194,8 +2194,8 @@ status_t AudioPolicyManagerCustom::startInput(audio_io_handle_t input,
                 String8 address = String8("");
                 if (inputDesc->mPolicyMix == NULL) {
                     address = String8("0");
-                } else if (inputDesc->mPolicyMix->mMixType == MIX_TYPE_PLAYERS) {
-                    address = inputDesc->mPolicyMix->mDeviceAddress;
+                } else if (inputDesc->mPolicyMix.promote()->mMixType == MIX_TYPE_PLAYERS) {
+                    address = inputDesc->mPolicyMix.promote()->mDeviceAddress;
                 }
                 if (address != "") {
                     setDeviceConnectionStateInt(AUDIO_DEVICE_OUT_REMOTE_SUBMIX,